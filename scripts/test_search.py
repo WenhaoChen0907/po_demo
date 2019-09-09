c16668326b9b8af6790f496d0718dc10cc8feaf9
WenhaoChen0907 <55017259+WenhaoChen0907@users.noreply.github.com>
2019-09-09 21:45:12 +0800

    测试完毕，恢复代码

--- scripts/test_search.py
+++ scripts/test_search.py
@@ -14,7 +14,7 @@ def data_with_key(key):
 
 class TestSearch:
     
-
+    """
     def setup(self):
         self.driver = init_driver()
         self.search_page = SearchPage(self.driver)
@@ -38,4 +38,4 @@ class TestSearch:
         allure.attach('输入密码', '密码有：1， 2,3')
         print("pass")
         assert 0
-     """
+     
